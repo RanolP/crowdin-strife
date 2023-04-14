d1dcfa4202cf8c96b5188478086ac64d7ec71f92
RanolP <me@ranolp.dev>
2023-04-14 22:20:56 +0900

    메시지 양식 변경 (#22)
    * reformat message
    
    * improve msgdata

--- crates/app/src/msgdata.rs
+++ crates/app/src/msgdata.rs
@@ -1,12 +1,22 @@
+use percent_encoding::{percent_decode_str, utf8_percent_encode, AsciiSet, CONTROLS};
+
+const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
 const MAGIC: &str = "https://data.store/";
 
-pub fn encode_msgdata(s: String) -> String {
-    format!("[]({MAGIC}{s})")
+pub fn encode_msgdata(s: &str) -> String {
+    format!(
+        "[ ]({MAGIC}{})",
+        utf8_percent_encode(&s, FRAGMENT).to_string()
+    )
 }
 
 pub fn decode_msgdata(s: &str) -> Option<String> {
     if let Some(index) = s.find(MAGIC) {
-        Some(s[index + MAGIC.len()..s.len() - 1].to_string())
+        Some(
+            percent_decode_str(&s[index + MAGIC.len()..s.len() - 1])
+                .decode_utf8_lossy()
+                .to_string(),
+        )
     } else {
         None
     }
